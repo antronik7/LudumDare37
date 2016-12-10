75947f5d916d4dd730f83357cadabaea4e961540
Flo <beneteau.florian34@gmail.com>
2016-12-10 15:49:18 -0500

    Ajout du script teleporterController

---
+++ LudumDare37/Assets/Script/TeleporteurController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleporteurController : MonoBehaviour {
+
+    public GameObject Player;
+    public GameObject SortieWrap;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Player.transform.position = SortieWrap.transform.position;
+        }
+    }
+}

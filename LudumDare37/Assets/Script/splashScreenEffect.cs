9b6ee2dc8ce2776333c1c7898aefd29f1befb341
Hugo Bombail <hugo.bombail@wanadoo.fr>
2016-12-12 21:36:47 -0500

    - Add des splashs screens

---
+++ LudumDare37/Assets/Script/splashScreenEffect.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class splashScreenEffect : MonoBehaviour {
+
+	public string nextScene;
+
+	void Start () {
+		StartCoroutine (startNextScene ());
+	}
+
+	void Update () {
+	
+	}
+
+	IEnumerator startNextScene(){
+		yield return new WaitForSeconds (3f);
+		SceneManager.LoadScene(nextScene, LoadSceneMode.Single);
+	}
+}
